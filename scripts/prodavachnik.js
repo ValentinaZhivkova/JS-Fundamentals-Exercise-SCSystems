424824370c571a00b09d7ad354b4426eda13e9ce
Valentina <valkisbg@gmail.com>
2017-06-14 11:34:23 +0300

    Stage 4, Task 1 - Notifications

--- scripts/prodavachnik.js
+++ scripts/prodavachnik.js
@@ -1,4 +1,8 @@
 function startApp() {
+    if (sessionStorage.getItem('authToken') !== null) {
+        let username = sessionStorage.getItem('username');
+        $('#loggedInUser').text("Welcome, " + username + "!");
+    }
     showHideMenuLinks();
     showHomeView('viewHome');
 
@@ -20,17 +24,19 @@ function startApp() {
     function showHideMenuLinks() {
         $("#linkHome").show();
         if (sessionStorage.getItem('authToken') === null) {
-            //No logged in user
+            // No logged in user
             $("#linkLogin").show();
             $("#linkRegister").show();
             $("#linkListAds").hide();
             $("#linkLogout").hide();
+            $("#loggedInUser").hide();
         } else {
-            //We have logged user
+            // We have logged in user
             $("#linkLogin").hide();
             $("#linkRegister").hide();
             $("#linkListAds").show();
             $("#linkLogout").show();
+            $("#loggedInUser").show();
         }
     }
 
@@ -87,7 +93,11 @@ function startApp() {
         sessionStorage.setItem('authToken', userAuth);
         let userId = userInfo._id;
         sessionStorage.setItem('userId', userId);
+        let username = userInfo.username;
+        sessionStorage.setItem('username', username);
+        $('#loggedInUser').text("Welcome, " + username + "!");
     }
+
 
     // user/register
     function registerUser() {
